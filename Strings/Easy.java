26ae2af80041bd71074ef49b4482d3be6365ff17
Harsha430 <harshasrikarthikeyathumuluri@gmail.com>
2025-12-09 09:36:26 +0530

    started strings

---
+++ Strings/Easy.java
@@ -0,0 +1,32 @@
+class Easy extends GuessGame {
+
+    public Easy(int picked) {
+        super(picked);
+    }
+
+    // * 1920. Build Array from Permutation
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+
+    // * 374. Guess Number Higher or Lower
+    public int guessNumber(int n) {
+        int start = 0;
+        int end = n;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (guess(mid) == 0) {
+                return mid;
+            } else if (guess(mid) == 1) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ Strings/GuessGame.java
@@ -0,0 +1,16 @@
+public class GuessGame {
+    private int picked;
+
+    public GuessGame(int picked) {
+        this.picked = picked;
+    }
+
+    public int guess(int num) {
+        if (num == picked)
+            return 0;
+        else if (num > picked)
+            return -1;
+        else
+            return 1;
+    }
+}
